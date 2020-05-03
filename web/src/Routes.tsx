7e31491914f948638c617e873a5e6294150cf40b
Crestock <47856835+Crestock@users.noreply.github.com>
2020-05-03 20:40:35 +0100

    WIP merging saas-template with apollo project

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import theme from "./UI/theme";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
@@ -9,15 +11,18 @@ import { Header } from "./Header";
 export const Routes: React.FC = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/bye" component={Bye} />
-        </Switch>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/bye" component={Bye} />
+          </Switch>
+        </div>
+      </MuiThemeProvider>
     </BrowserRouter>
   );
 };
